78589a6551e7c88e80e138cbedb5f1f6f2eacf06
9-max-6 <mutukumaxwel@gmail.com>
2024-03-27 09:31:40 +0300

    Fix: use the make node functio

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,26 +16,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL);
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (!new_node)
 		return (NULL);
-	new_node->n = value;
-	if (parent->left == NULL)
-	{
-		parent->left = new_node;
-		new_node->right = NULL;
-		new_node->left = NULL;
-		return (new_node);
-	}
 
 	if (parent->left)
 	{
-		parent->left->parent = new_node;
 		new_node->left = parent->left;
-		parent->left = new_node;
-		new_node->right = NULL;
+		parent->left->parent = new_node;
 	}
-	new_node->parent = parent;
-
+	parent->left = new_node;
 	return (new_node);
 }

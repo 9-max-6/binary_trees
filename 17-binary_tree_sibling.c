90a00ff531b6e5a285b53149ac34e0bc24e7b945
9-max-6 <mutukumaxwel@gmail.com>
2024-03-27 09:15:08 +0300

    Fix: null parent

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,10 +8,15 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node)
+	if (!node || !node->parent)
 		return (NULL);
+
 	if (node->parent->left == node)
+	{
+		if (!node->parent->right)
+			return (NULL);
 		return (node->parent->right);
+	}
 	else
 		return (node->parent->left);
 }
